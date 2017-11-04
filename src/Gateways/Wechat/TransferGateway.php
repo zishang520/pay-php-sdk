56701396817d2d2d29324fad4f0cf1cc70f8abba
Anyon <zoujingli@qq.com>
2017-11-04 18:28:11 +0800

    [更新]去除调试代码

--- src/Gateways/Wechat/TransferGateway.php
+++ src/Gateways/Wechat/TransferGateway.php
@@ -48,7 +48,6 @@ class TransferGateway extends Wechat
         unset($this->config['notify_url']);
         $this->config = array_merge($this->config, $options);
         $this->config['sign'] = $this->getSign($this->config);
-        var_export($this->config);
         $data = $this->fromXml($this->post(
             $this->gateway_transfer, $this->toXml($this->config),
             ['ssl_cer' => $this->userConfig->get('ssl_cer', ''), 'ssl_key' => $this->userConfig->get('ssl_key', '')]
