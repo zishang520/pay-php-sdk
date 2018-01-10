eae5dd311911332e8d4b22da5f9ab5746e4acebd
Anyon <zoujingli@qq.com>
2018-01-10 16:22:16 +0800

    [更新]修改加载注册机制

--- init.php
+++ init.php
@@ -12,9 +12,11 @@
 // | 项目设计及部分源码参考于 yansongda/pay，在此特别感谢！
 // +----------------------------------------------------------------------
 
+// 注册类库自动加载, 适用于普通加载
 spl_autoload_register(function ($class) {
-    if (0 === stripos($class, 'Pay\\')) {
-        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace(['\\', 'Pay/',], ['/', 'src/'], $class) . '.php';
-        file_exists($filename) && include($filename);
+    if (stripos($class, 'Pay\\') === 0) {
+        list($search, $replace) = [['\\', 'Pay/'], ['/', 'src/']];
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace($search, $replace, $class) . '.php';
+        file_exists($filename) && include $filename;
     }
 });
